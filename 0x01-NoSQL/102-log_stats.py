220692224340747d355bc0e151126253ab2e5aa9
yas19sin <yas19sin@gmail.com>
2025-05-07 05:47:15 +0100

    fixes

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -14,20 +14,20 @@ if __name__ == "__main__":
 
     # Count total logs
     total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
+    print("{} logs".format(total_logs))
 
     # Count methods
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        print("    method {}: {}".format(method, count))
 
     # Count status checks
     status_checks = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print(f"{status_checks} status check")
+    print("{} status check".format(status_checks))
 
     # Get top 10 IPs
     print("IPs:")
@@ -39,4 +39,4 @@ if __name__ == "__main__":
     top_ips = nginx_collection.aggregate(pipeline)
 
     for ip_data in top_ips:
-        print(f"    {ip_data.get('_id')}: {ip_data.get('count')}")
+        print("    {}: {}".format(ip_data.get('_id'), ip_data.get('count')))
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -14,17 +14,17 @@ if __name__ == "__main__":
 
     # Count total logs
     total_logs = nginx_collection.count_documents({})
-    print(f"{total_logs} logs")
+    print("{} logs".format(total_logs))
 
     # Count methods
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        print("    method {}: {}".format(method, count))
 
     # Count status checks
     status_checks = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print(f"{status_checks} status check")
+    print("{} status check".format(status_checks))
